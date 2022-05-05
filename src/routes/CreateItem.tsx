8382115fc03492afbc2b2cbeff3ff8d0bfe4a589
bauele <bauele@users.noreply.github.com>
2022-05-05 13:56:53 -0400

    Fix bug with item detail buttons not properly styling after toggle

--- src/routes/CreateItem.tsx
+++ src/routes/CreateItem.tsx
@@ -1,81 +1,124 @@
-import '../styles/CreateItem.css';
-import ItemEntryForm from '../components/ItemEntryForm';
-import { useState } from 'react';
-import { serverSingleton } from '../api/ServerAPI';
+import "../styles/CreateItem.css";
+import ItemEntryForm from "../components/ItemEntryForm";
+import { useState } from "react";
+import { serverSingleton } from "../api/ServerAPI";
 
 let itemDetailsDefault = new Map<string, boolean>();
-itemDetailsDefault.set('nutrition-button', false);
-itemDetailsDefault.set('spending-button', false);
-itemDetailsDefault.set('supply-button', false);
+itemDetailsDefault.set("nutrition-button", false);
+itemDetailsDefault.set("spending-button", false);
+itemDetailsDefault.set("supply-button", false);
 
 type Props = {
-    accountEmail: string | null;
-}
+  accountEmail: string | null;
+};
 
-export default function CreateItem({accountEmail}: Props) {
-    const [selectedItemDetails,setSelectedItemDetails] = useState(new Map<string, boolean>(itemDetailsDefault));
-    const [showItemDetailsButtons, setShowItemDetailsButtons] = useState(true);
-    const [showItemForm, setShowItemForm] = useState(false);
+export default function CreateItem({ accountEmail }: Props) {
+  const [selectedItemDetails, setSelectedItemDetails] = useState(
+    new Map<string, boolean>(itemDetailsDefault)
+  );
+  const [showItemDetailsButtons, setShowItemDetailsButtons] = useState(true);
+  const [showItemForm, setShowItemForm] = useState(false);
 
-    function toggleItemDetail(event: React.MouseEvent<HTMLElement>) {
-        const buttonId = event.currentTarget.id;
+  function toggleItemDetail(event: React.MouseEvent<HTMLElement>) {
+    const buttonId = event.currentTarget.id;
 
-        let newItemDetails = selectedItemDetails;
-        newItemDetails.set(buttonId, !newItemDetails.get(buttonId));
+    let newItemDetails = selectedItemDetails;
+    newItemDetails.set(buttonId, !newItemDetails.get(buttonId));
 
-        // Toggles the border around the button to show whether or not
-        // the selected item detail has been enabled
-        newItemDetails.get(buttonId) ?  
-            document.querySelector(`#${buttonId}`)?.classList.add("item-details-button-selected")
-            : document.querySelector(`#${buttonId}`)?.classList.remove("item-details-button-selected")
+    // Toggles the border around the button to show whether or not
+    // the selected item detail has been enabled
+    newItemDetails.get(buttonId)
+      ? document
+          .querySelector(`#${buttonId}`)
+          ?.classList.add("item-details-button-selected")
+      : document
+          .querySelector(`#${buttonId}`)
+          ?.classList.remove("item-details-button-selected");
 
-        setSelectedItemDetails(newItemDetails);
+    setSelectedItemDetails(newItemDetails);
+  }
+
+  function toggleFormPage() {
+    setShowItemDetailsButtons(!showItemDetailsButtons);
+    setShowItemForm(!showItemForm);
+
+    // Disable all of the item detail tracking
+    itemDetailsDefault.set("nutrition-button", false);
+    itemDetailsDefault.set("spending-button", false);
+    itemDetailsDefault.set("supply-button", false);
+    setSelectedItemDetails(itemDetailsDefault);
+  }
+
+  function submitForm(item: {}) {
+    alert("Submitted your item!");
+    console.log("Item = ", item);
+
+    if (typeof accountEmail === "string") {
+      serverSingleton.createItem(accountEmail, item);
     }
+  }
 
-    function toggleFormPage() {
-        setShowItemDetailsButtons(!showItemDetailsButtons);
-        setShowItemForm(!showItemForm);
+  function renderItemDetailsButtons() {
+    if (showItemDetailsButtons) {
+      return (
+        <>
+          <h2 id="details-prompt">
+            What details do you want to track about this item?
+          </h2>
+          <button
+            id="nutrition-button"
+            className="item-details-button"
+            onClick={toggleItemDetail}
+          >
+            Nutrition
+          </button>
+          <button
+            id="spending-button"
+            className="item-details-button"
+            onClick={toggleItemDetail}
+          >
+            Spending
+          </button>
+          <button
+            id="supply-button"
+            className="item-details-button"
+            onClick={toggleItemDetail}
+          >
+            At-Home Supply
+          </button>
+          <button
+            id="continue-button"
+            className="item-details-button"
+            onClick={toggleFormPage}
+          >
+            Continue
+          </button>
+        </>
+      );
     }
+  }
 
-    function submitForm(item: {}) {
-        alert("Submitted your item!");
-        console.log('Item = ', item);
-
-        if (typeof accountEmail === 'string') {
-            serverSingleton.createItem(accountEmail, item);
-        }
+  function renderItemForm() {
+    if (showItemForm) {
+      return (
+        <>
+          <ItemEntryForm
+            selectedItemDetails={selectedItemDetails}
+            submitForm={submitForm}
+          />
+          <button id="back-button" onClick={toggleFormPage}>
+            Go Back
+          </button>
+        </>
+      );
     }
+  }
+  return (
+    <div id="parent">
+      <h1>Create Item</h1>
 
-    function renderItemDetailsButtons() {
-        if (showItemDetailsButtons) {
-            return (
-                <>
-                    <h2 id="details-prompt">What details do you want to track about this item?</h2>
-                    <button id="nutrition-button" className="item-details-button" onClick={toggleItemDetail}>Nutrition</button>
-                    <button id="spending-button" className="item-details-button" onClick={toggleItemDetail}>Spending</button>
-                    <button id="supply-button" className="item-details-button" onClick={toggleItemDetail}>At-Home Supply</button>
-                    <button id="continue-button" className="item-details-button" onClick={toggleFormPage} >Continue</button>
-                </>
-            )
-        }
-    }
-
-    function renderItemForm() {
-        if (showItemForm) {
-            return (
-                <>
-                    <ItemEntryForm selectedItemDetails={selectedItemDetails} submitForm={submitForm}/>
-                    <button id="back-button" onClick={toggleFormPage}>Go Back</button>
-                </>
-            )
-        }
-    }
-    return (
-        <div id="parent">
-            <h1>Create Item</h1>
-
-            {renderItemDetailsButtons()}
-            {renderItemForm()}
-        </div>
-    )
+      {renderItemDetailsButtons()}
+      {renderItemForm()}
+    </div>
+  );
 }
