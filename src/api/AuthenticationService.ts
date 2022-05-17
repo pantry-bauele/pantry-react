467fd00e752ddf71b45e9b5fbf31fee54d852454
bauele <bauele@users.noreply.github.com>
2022-05-16 23:34:41 -0400

    Apply auto formatting rules to AuthenticationService.ts

--- src/api/AuthenticationService.ts
+++ src/api/AuthenticationService.ts
@@ -1,6 +1,6 @@
 import { getAuth, signOut } from "firebase/auth";
-import firebase from 'firebase/compat/app';
-import * as firebaseui from 'firebaseui'
+import firebase from "firebase/compat/app";
+import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXAhW8XBVJ4mmuUi_2NaDRpMY_MYDCwn8",
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: "pantry-be236.appspot.com",
   messagingSenderId: "866075198626",
   appId: "1:866075198626:web:770e72230b22f34bf3c3fb",
-  measurementId: "G-6LW3GB395J"
+  measurementId: "G-6LW3GB395J",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
@@ -20,23 +20,26 @@ export function startLoginUI() {
     signInOptions: [
       {
         provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayName: false
-      }
+        requireDisplayName: false,
+      },
     ],
     callbacks: {
-      signInSuccessWithAuthResult: function (authResult: any, redirectUrl: any) {
+      signInSuccessWithAuthResult: function (
+        authResult: any,
+        redirectUrl: any
+      ) {
         return true;
       },
     },
-    signInSuccessUrl: '/createItem'
-  }
+    signInSuccessUrl: "/createItem",
+  };
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+  ui.start("#firebaseui-auth-container", uiConfig);
 }
 
 export function logoutUser() {
   signOut(getAuth());
-  localStorage.removeItem('loggedIn');
+  localStorage.removeItem("loggedIn");
 }
 
-export default {}
+export default {};
