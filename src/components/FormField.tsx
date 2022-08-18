96960fadbbb250933832f6bbd09e28badfe8c1d8
bauele <bauele@users.noreply.github.com>
2022-08-18 17:07:24 -0400

    Refactor FormField and FormSelectField

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -4,9 +4,9 @@ import "../styles/sass/FormField.css";
 interface Props {
   label: string;
   orientation: "horizontal" | "vertical";
+
   onChange?: ChangeEventHandler<HTMLInputElement>;
   onBlur?: FocusEventHandler<HTMLInputElement>;
-  id?: string;
   name?: string;
   hideInput?: boolean;
   value?: string;
@@ -17,43 +17,20 @@ export const FormField = ({
   orientation,
   onChange,
   onBlur,
-  id,
   name,
   hideInput,
-  value,
 }: Props) => {
-  if (orientation === "vertical") {
-    return (
-      <div id="form-container-vertical">
-        <div id="form-label-vertical">{label}</div>
-        <input
-          className="form-text-vertical"
-          id={`form-text-${name}`}
-          name={name}
-          onChange={onChange}
-          onBlur={onBlur}
-          type={hideInput ? "password" : "text"}
-        />
-      </div>
-    );
-  }
-
-  if (orientation === "horizontal") {
-    return (
-      <div id="form-container-horizontal">
-        <div id="form-label-horizontal">{label}</div>
-        <input
-          className="form-text-horizontal"
-          id={`form-text-${name}`}
-          name={name}
-          onChange={onChange}
-          onBlur={onBlur}
-          type={hideInput ? "password" : "text"}
-          value={value}
-        />
-      </div>
-    );
-  }
-
-  return <></>;
+  return (
+    <div className={`form-container-${orientation}`}>
+      <div className={`form-label-${orientation}`}>{label}</div>
+      <input
+        className={`form-text-${orientation}`}
+        id={`form-text-${name}`}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        type={hideInput ? "password" : "text"}
+      />
+    </div>
+  );
 };
--- src/components/FormSelectField.tsx
+++ src/components/FormSelectField.tsx
@@ -1,34 +1,21 @@
-import {
-  ChangeEvent,
-  ChangeEventHandler,
-  FocusEventHandler,
-  FormEventHandler,
-} from "react";
+import { ChangeEventHandler } from "react";
 import "../styles/sass/FormSelectField.css";
 
 interface Props {
   label: string;
-  onChange?: ChangeEventHandler<HTMLSelectElement>;
-  onBlur?: FocusEventHandler<HTMLInputElement>;
-  id?: string;
-  name?: string;
-  hideInput?: boolean;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+  name: string;
   options: string[];
-  value?: string;
+  value: string;
 }
 
 export const FormSelectField = ({
   label,
   onChange,
-  onBlur,
-  id,
   options,
   name,
-  hideInput,
   value,
 }: Props) => {
-  console.log(options);
-
   let selectElements = options.map((optionName, index) => {
     return (
       <option key={index} value={optionName}>
@@ -37,7 +24,6 @@ export const FormSelectField = ({
     );
   });
 
-  console.log(selectElements);
   return (
     <div id="form-select-container">
       <select
