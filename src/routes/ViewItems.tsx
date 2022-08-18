11821126273032023838e09fd2ff70a73e7b3d23
bauele <bauele@users.noreply.github.com>
2022-08-18 08:29:03 -0400

    Perform general refactoring on ViewItems
    Several pieces of commented out or unnecessary code have also been
    removed. Element ids have been renamed for consistency. The stylesheet
    has been moved into a Sass sheet.

--- src/routes/ViewItems.tsx
+++ src/routes/ViewItems.tsx
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
-import Item from "../components/items/Item";
 
-import { ServerAPI, serverSingleton } from "../api/ServerAPI";
-import { ItemBuilder } from "../pantry-shared/src/itemBuilder";
+import Item from "../components/items/Item";
 import { Item as ItemObject } from "../pantry-shared/src/item";
-import "../styles/ViewItems.css";
+import { ItemBuilder } from "../pantry-shared/src/itemBuilder";
 import { PantryItem } from "../pantry-shared/src/pantryItem";
 import { AddPantryItemModal } from "../components/AddPantryItemModal";
+import { serverSingleton } from "../api/ServerAPI";
+
+import "../styles/sass/ViewItems.css";
 
 interface Props {
   accountEmail: string | null;
 }
 
-export default function ViewItems(props: Props) {
-  const [accountEmail, setAccountEmail] = useState(props.accountEmail);
-  const [listItems, setListItems] = useState("");
+export const ViewItems = ({ accountEmail }: Props) => {
+  const [finishedLoading, setFinishedLoading] = useState(false);
+  const [listItems, setListItems] = useState(new Array<JSX.Element>());
   const [showAddPantryItemModal, setShowAddPantryItemModal] = useState(false);
-  const [itemBeingAddedToPantry, setItemBeingAddedToPantry] = useState({});
+  const [itemBeingAddedToPantry, setItemBeingAddedToPantry] = useState(
+    new ItemObject()
+  );
 
-  console.log(props.accountEmail);
-
-  async function loadItems(emailAddress: string | null) {
-    console.log(`Loading items from ${emailAddress}`);
+  const loadItems = async (accountEmail: string | null) => {
+    console.log(`Loading items from ${accountEmail}`);
 
     let response;
-    if (typeof emailAddress === "string") {
-      response = await serverSingleton.loadItems(emailAddress);
+    if (typeof accountEmail === "string") {
+      response = await serverSingleton.loadItems(accountEmail);
     }
 
     console.log("response = ", response);
@@ -33,89 +34,62 @@ export default function ViewItems(props: Props) {
       console.log(response);
 
       let itemBuilder = new ItemBuilder();
-      const elements = response.map((element: any) => (
-        <Item
-          key={itemBuilder.buildItem(element).getId()}
-          item={itemBuilder.buildItem(element)}
-          deleteItem={deleteItem}
-          addItem={addItem}
-        />
-      ));
+      const elements: Array<JSX.Element> = response.map(
+        (element: ItemObject) => (
+          <Item
+            key={itemBuilder.buildItem(element).getId()}
+            item={itemBuilder.buildItem(element)}
+            deleteItem={deleteItem}
+            addItem={addItem}
+          />
+        )
+      );
 
       console.log(elements);
       setListItems(elements);
+      setFinishedLoading(true);
     }
-  }
+  };
 
-  async function addItem(item: ItemObject) {
+  const addItem = async (item: ItemObject) => {
     setItemBeingAddedToPantry(item);
     setShowAddPantryItemModal(true);
-  }
+  };
 
-  async function deleteItem(item: {}) {
+  const deleteItem = async (item: ItemObject) => {
     alert(`Deleted item`);
-    console.log(item);
 
     if (typeof accountEmail === "string") {
       await serverSingleton.deleteItem(accountEmail, item);
-    } else {
-      console.log("accountEmail is not a string!");
+      await loadItems(accountEmail);
     }
+  };
 
-    await loadItems(accountEmail);
-
-    // Eventually, add support for removing that particular
-    // item from the array here
-  }
-
-  function closeModal() {
+  const closeModal = () => {
     setShowAddPantryItemModal(false);
-    setItemBeingAddedToPantry({});
-  }
+    setItemBeingAddedToPantry(new ItemObject());
+  };
 
-  async function submitModal(
+  const submitModal = async (
     quantity: string,
     quantityUnit: string,
-    expirationDate: any
-  ) {
-    console.log(itemBeingAddedToPantry);
-    console.log(quantity);
-    console.log(quantityUnit);
-    console.log(expirationDate);
-
+    expirationDate: number
+  ) => {
     if (itemBeingAddedToPantry instanceof ItemObject) {
       let pantryItem = new PantryItem(itemBeingAddedToPantry);
-
-      /*
-      if (quantity !== "") {
-        pantryItem.setAvailableQuantity(
-          Number.parseInt(quantity),
-          quantityUnit
-        );
-
-        console.log("parsed = ", Number.parseInt(quantity));
-      }
-      */
 
       let itemQuantity = itemBeingAddedToPantry.getTotalQuantity();
       if (itemQuantity.amount > 0) {
         pantryItem.setAvailableQuantity(itemQuantity.amount, itemQuantity.unit);
       }
 
-      console.log("preExpiration = ", expirationDate);
       if (expirationDate !== 0) {
-        console.log("expiration date is ", expirationDate);
-
         let dateString = new Date(expirationDate);
         let monthIndex = dateString.getMonth();
-        let day = dateString.getUTCDate(); // Must be to avoid time-zone day errors
+        let day = dateString.getUTCDate(); // Must be UTCDate to avoid time-zone day errors
         let year = dateString.getFullYear();
 
         pantryItem.setExpirationDate(year, monthIndex, day);
-        console.log(
-          "pantryItem.getExpirationDate() = ",
-          pantryItem.getExpirationDate()
-        );
       }
 
       if (accountEmail !== null) {
@@ -124,23 +98,24 @@ export default function ViewItems(props: Props) {
     }
 
     alert("Added item to pantry!");
-  }
+  };
 
   useEffect(() => {
     loadItems(accountEmail);
   }, [accountEmail]);
 
-  function renderNoItems() {
-    if (listItems.length === 0) {
-      return <div id="no-items-text">You haven't added any items yet!</div>;
-    }
-  }
-
   return (
     <div id="view-items-container">
-      <h1 id="items-heading">Items</h1>
-      {renderNoItems()}
-      <div id="items"> {listItems} </div>
+      <h1 id="view-items-heading">Items</h1>
+      {!finishedLoading && (
+        <div id="view-items-loading-text">Loading your items...</div>
+      )}
+      {finishedLoading && listItems.length === 0 && (
+        <div id="view-items-no-items-text">
+          You haven't added any items yet!
+        </div>
+      )}
+      <div id="view-items-elements"> {listItems} </div>
       <AddPantryItemModal
         isOpen={showAddPantryItemModal}
         closeModal={closeModal}
@@ -148,4 +123,4 @@ export default function ViewItems(props: Props) {
       />
     </div>
   );
-}
+};
