b5b16a29bbe16535e61a594b42ca35b751372556
bauele <bauele@users.noreply.github.com>
2022-08-22 22:39:20 -0400

    Introduce first horizontal breakpoint to resize the Home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,29 +12,31 @@ export const Home = () => {
 
   return (
     <div id="home-page-container">
-      <div id="hero-container">
-        <div id="hero-info-container">
-          <div>
-            <h2>Take control of your food</h2>
-            <h3>Discover the power behind grocery analytics</h3>
+      <div id="hero-promo-banner-horizontal-container">
+        <div id="hero-container">
+          <div id="hero-info-container">
+            <div>
+              <h2>Take control of your food</h2>
+              <h3>Discover the power behind grocery analytics</h3>
+            </div>
+
+            <div id="hero-button-container">
+              <Button
+                className="brand-button-white button-medium clickable-button"
+                text="Sign Up"
+                click={redirectToSignUp}
+              ></Button>
+            </div>
           </div>
 
-          <div id="hero-button-container">
-            <Button
-              className="brand-button-white button-medium clickable-button"
-              text="Sign Up"
-              click={redirectToSignUp}
-            ></Button>
+          <div id="hero-logo-container">
+            <div id="hero-logo"></div>
           </div>
         </div>
 
-        <div id="hero-logo-container">
-          <div id="hero-logo"></div>
+        <div id="promo-banner-container">
+          <div id="promo-banner-image"></div>
         </div>
-      </div>
-
-      <div id="promo-banner-container">
-        <div id="promo-banner-image"></div>
       </div>
 
       <div id="bottom-action-container">
